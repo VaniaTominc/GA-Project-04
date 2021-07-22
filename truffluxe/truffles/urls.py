0fa01d593f0fe76b734b08c636e28af062147c93
vtominc <vanja.tominc@gmail.com>
2021-07-22 16:02:34 +0100

    Get one truffle

--- truffluxe/truffles/urls.py
+++ truffluxe/truffles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TruffleAllView
+from .views import TruffleAllView, TruffleDetailView
 
 urlpatterns = [
-    path('', TruffleAllView.as_view())
+    path('', TruffleAllView.as_view()),
+    path('<int:pk>/', TruffleDetailView.as_view())
 ]
--- truffluxe/truffles/views.py
+++ truffluxe/truffles/views.py
@@ -1,6 +1,8 @@
-from rest_framework.views import APIView      # Sends back json
-from rest_framework.response import Response   # Sends back response
-from rest_framework import status             # Sends back a status code
+from rest_framework.views import APIView        # Sends back json
+from rest_framework.response import Response    # Sends back response
+from rest_framework import status               # Sends back a status code
+
+from rest_framework.exceptions import NotFound  # Error handling
 
 from .models import Truffle
 from .serializers import TruffleSerializer
@@ -11,3 +13,16 @@ class TruffleAllView(APIView):
         truffles = Truffle.objects.all()
         serialized_truffles = TruffleSerializer(truffles, many=True)
         return Response(serialized_truffles.data, status=status.HTTP_200_OK)
+
+class TruffleDetailView(APIView):
+
+    def get_truffle(self, pk):
+        try:
+            return Truffle.objects.get(pk=pk)
+        except:
+            raise NotFound(detail="💎 Can't find that underground treasure!")
+
+    def get(self, _request, pk):
+        truffle = self.get_truffle(pk=pk)
+        serialized_truffle = TruffleSerializer(truffle)
+        return Response(serialized_truffle.data, status=status.HTTP_200_OK)
