32849c9fb03226de8a108f7f791162f160e8c489
vtominc <vanja.tominc@gmail.com>
2021-07-22 22:16:15 +0100

    Opinions - edit, get one, get all work

--- truffluxe/opinions/views.py
+++ truffluxe/opinions/views.py
@@ -7,6 +7,13 @@ from .serializers.common import OpinionSerializer
 from opinions.models import Opinion
 
 class OpinionListView(APIView):
+
+    # GET request all
+    def get(self, _request):
+        opinions = Opinion.objects.all()
+        serialized_opinions = OpinionSerializer(opinions, many=True)
+        return Response(serialized_opinions.data, status=status.HTTP_200_OK)
+
     # POST request
     def post(self, request):
 
@@ -18,12 +25,33 @@ class OpinionListView(APIView):
         return Response(opinion_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 class OpinionDetailView(APIView):
+
+    def get_opinion(self, pk):
+        try:
+            return Opinion.objects.get(pk=pk)
+        except:
+            raise NotFound(detail="🆘 Can't find that comment 😱.")
+
+    # GET request one
+    def get(self, _request, pk):
+
+        opinion = self.get_opinion(pk=pk)
+        serialized_opinion = OpinionSerializer(opinion)
+        return Response(serialized_opinion.data, status=status.HTTP_200_OK)
+
+    # EDIT request
+    def put(self, request, pk):
+
+        opinion_to_edit = self.get_opinion(pk=pk)
+        updated_opinion = OpinionSerializer(opinion_to_edit, data=request.data)
+        if updated_opinion.is_valid():
+            updated_opinion.save()
+            return Response(updated_opinion.data, status=status.HTTP_202_ACCEPTED)
+        return Response(updated_opinion.data, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
     # DELETE request
     def delete(self, request, pk):
 
-        try:
-            opinion_to_delete = Opinion.objects.get(pk=pk)
-        except Opinion.DoesNotExist:
-            raise NotFound(detail="😱 Comment not found")
+        opinion_to_delete = self.get_opinion(pk=pk)
         opinion_to_delete.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
