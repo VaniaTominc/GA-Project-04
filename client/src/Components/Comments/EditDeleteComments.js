7e186a1c8d2bb54cc2f86c2f8148a56c808eade3
vtominc <vanja.tominc@gmail.com>
2021-08-13 02:24:14 +0100

    redeploying

--- client/src/Components/Comments/EditDeleteComments.js
+++ client/src/Components/Comments/EditDeleteComments.js
@@ -151,7 +151,7 @@ const EditDeleteComments = () => {
                                   <input 
                                     type='submit' 
                                     value='Update' 
-                                    // onClick={handleCommentChangeSubmit}
+                                    onClick={handleCommentChangeSubmit}
                                     className='delete-buttons-space'
                                   />
                                   <input 
